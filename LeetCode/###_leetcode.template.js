aad77105f2c82a1d63919ab593b20fe3bb3dd33a
Christian Mejia <chrismejia10@gmail.com>
2023-01-22 16:02:24 -0500

    feat(LC): add leetcode JS template file

---
+++ LeetCode/###_leetcode.template.js
@@ -0,0 +1,30 @@
+/**
+ * @see {@link https://leetcode.comlink ###. PROBLEM NAME GOES HERE}
+ */
+
+/**
+ * EXPLAIN
+ */
+
+/**
+ * APPROACH
+ */
+
+/**
+ * CODE
+ */
+
+/**
+ * COMPLEXITY
+ * TIME   O()
+ * SPACE  O()
+ */
+
+/**
+ * READ       0
+ * EXPLAIN    0
+ * APPROACH   0
+ * CODE       0
+ * TEST       0
+ * OPTIMIZE   0
+ */
