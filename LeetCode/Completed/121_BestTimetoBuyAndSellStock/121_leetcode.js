113cf34dc53194330ed6b2a60ad5f77f7f340d05
Christian Mejia <chrismejia10@gmail.com>
2023-01-25 21:30:11 -0500

    feat(LC): add 121 and 142 base files; no code

---
+++ LeetCode/Completed/121_BestTimetoBuyAndSellStock/121_leetcode.js
@@ -0,0 +1,32 @@
+/**
+ * @see {@link https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan&id=level-1 121. Best Time to Buy and Sell Stock}
+ */
+
+/**
+ * EXPLAIN
+ */
+
+/**
+ * APPROACH
+ */
+
+/**
+ * CODE
+ * TIME:
+ * MEMORY:
+ */
+
+/**
+ * COMPLEXITY
+ * TIME   O()
+ * SPACE  O()
+ */
+
+/**
+ * READ             0
+ * EXPLAIN          0
+ * APPROACH         0
+ * CODE [FAIL]      0
+ * TEST             0
+ * OPTIMIZE         0
+ */
---
+++ LeetCode/Completed/142_LinkedListCycleII/142_leetcode.js
@@ -0,0 +1,32 @@
+/**
+ * @see {@link https://leetcode.com/problems/linked-list-cycle-ii/?envType=study-plan&id=level-1 142. LinkedListCycleII}
+ */
+
+/**
+ * EXPLAIN
+ */
+
+/**
+ * APPROACH
+ */
+
+/**
+ * CODE
+ * TIME:
+ * MEMORY:
+ */
+
+/**
+ * COMPLEXITY
+ * TIME   O()
+ * SPACE  O()
+ */
+
+/**
+ * READ             0
+ * EXPLAIN          0
+ * APPROACH         0
+ * CODE [FAIL]      0
+ * TEST             0
+ * OPTIMIZE         0
+ */
